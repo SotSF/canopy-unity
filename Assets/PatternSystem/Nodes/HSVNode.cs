40cc815f3dc71833441ee6ae905d9e4ca14b2596
Kevin Reynolds <tossrock@gmail.com>
2018-09-30 16:44:26 -0700

    WIP: Integrating a node editor framework

---
+++ Assets/PatternSystem/Nodes/HSVNode.cs
@@ -0,0 +1,81 @@
+﻿using NodeEditorFramework;
+using NodeEditorFramework.TextureComposer;
+using UnityEngine;
+
+
+[Node(false, "Pattern/HSVNode")]
+public class HSVNode : Node
+{
+    public const string ID = "hsvNode";
+    public override string GetID { get { return ID; } }
+
+    public override string Title { get { return "HSV"; } }
+    public override Vector2 DefaultSize { get { return new Vector2(150, 100); } }
+
+    [ValueConnectionKnob("Texture", Direction.In, typeof(Texture))]
+    public ValueConnectionKnob textureInputKnob;
+
+    [ValueConnectionKnob("Texture", Direction.Out, typeof(Texture))]
+    public ValueConnectionKnob textureOutputKnob;
+
+    [ValueConnectionKnob("H", Direction.In, "Float")]
+    public ValueConnectionKnob hueKnob;
+    [ValueConnectionKnob("S", Direction.In, "Float")]
+    public ValueConnectionKnob satKnob;
+    [ValueConnectionKnob("V", Direction.In, "Float")]
+    public ValueConnectionKnob valKnob;
+
+    private ComputeShader HSVShader;
+    private int kernelId;
+    private Vector4 HSV;
+    public RenderTexture outputTex;
+
+    private void Awake()
+    {
+        HSVShader = Resources.Load<ComputeShader>("Filters/HSVFilter");
+        kernelId = HSVShader.FindKernel("CSMain");
+        outputTex = new RenderTexture(Constants.PIXELS_PER_STRIP, Constants.NUM_STRIPS, 24);
+        outputTex.enableRandomWrite = true;
+        outputTex.Create();
+    }
+
+    public override void NodeGUI()
+    {
+        GUILayout.BeginHorizontal();
+
+        GUILayout.BeginVertical();
+        textureInputKnob.DisplayLayout();
+        hueKnob.DisplayLayout();
+        satKnob.DisplayLayout();
+        valKnob.DisplayLayout();
+        GUILayout.EndVertical();
+
+        textureOutputKnob.DisplayLayout();
+
+        GUILayout.EndHorizontal();
+
+        if (GUI.changed)
+            NodeEditor.curNodeCanvas.OnNodeChange(this);
+    }
+
+    public override bool Calculate()
+    {
+        Texture tex = textureInputKnob.GetValue<Texture>();
+        if (!textureInputKnob.connected() || tex == null)
+        { // Reset outputs if no texture is available
+            textureOutputKnob.ResetValue();
+            return true;
+        }
+        HSV = new Vector4(hueKnob.GetValue<float>(), satKnob.GetValue<float>(), valKnob.GetValue<float>());
+        //Execute HSV compute shader here
+        HSVShader.SetVector("HSV", HSV);
+        HSVShader.SetTexture(kernelId, "OutputTex", outputTex);
+        HSVShader.SetTexture(kernelId, "InputTex", textureInputKnob.GetValue<Texture>());
+        HSVShader.Dispatch(kernelId, Constants.PIXELS_PER_STRIP / 25, Constants.NUM_STRIPS / 16, 1);
+
+        // Assign output channels
+        textureOutputKnob.SetValue(outputTex);
+
+        return true;
+    }
+}
---
+++ Assets/Plugins/Node_Editor/Examples/Texture_Composer/Nodes/TextureInfoNode.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace NodeEditorFramework.TextureComposer
+{
+	[Node(false, "Texture/Info")]
+	public class TextureInfoNode : Node
+	{
+		public const string ID = "texInfoNode";
+		public override string GetID { get { return ID; } }
+
+		public override string Title { get { return "Texture Info"; } }
+		public override Vector2 MinSize { get { return new Vector2(150, 50); } }
+		public override bool AutoLayout { get { return true; } }
+
+		[ValueConnectionKnob("Texture", Direction.In, typeof(Texture))]
+		public ValueConnectionKnob inputKnob;
+
+		[System.NonSerialized]
+		public Texture tex;
+
+		public override void NodeGUI()
+		{
+			inputKnob.DisplayLayout(new GUIContent("Texture" + (tex != null ? ":" : " (null)"), "The texture to display information about."));
+			if (tex != null)
+			{
+				RTTextureViz.DrawTexture(tex, 64);
+				GUILayout.Label("'" + tex.name + "'");
+				GUILayout.Label("Size: " + tex.width + "x" + tex.height + "");
+			}
+		}
+
+		public override bool Calculate()
+		{
+			tex = inputKnob.connected() ? inputKnob.GetValue<Texture>() : null;
+			return true;
+		}
+	}
+
+}
