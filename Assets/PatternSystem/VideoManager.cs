ad716f54e9b096e13bfa82445e4aae343873804a
Kevin Reynolds <tossrock@gmail.com>
2018-10-08 19:07:22 -0700

    Video manager stuff

--- Assets/PatternSystem/VideoManager.cs
+++ Assets/PatternSystem/VideoManager.cs
@@ -1,13 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Video;
 
 public class VideoManager : MonoBehaviour
 {
+    private VideoClip[] animatedTextures;
+
 
     // Use this for initialization
     void Start()
     {
+        animatedTextures = Resources.LoadAll<VideoClip>("AnimatedTextures");
+        foreach (var anim in animatedTextures)
+        {
+            var player = gameObject.AddComponent<VideoPlayer>();
+            player.clip = anim;
+            player.renderMode = VideoRenderMode.RenderTexture;
 
+        }
     }
 
     // Update is called once per frame
