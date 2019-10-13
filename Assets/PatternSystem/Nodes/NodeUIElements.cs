4a186450288220416b17d2b2fb5f1451ce7b3160
Kevin Reynolds <tossrock@gmail.com>
2019-10-12 23:52:36 -0700

    Basic NodeUIElements static class for reused UI patterns, TextureBankNode for dynamic list of available textures

---
+++ Assets/PatternSystem/Nodes/NodeUIElements.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class NodeUIElements
+{
+    public static void TexInfo(Texture tex, float width=0, float height=0, bool showAttribs=true)
+    {
+        GUILayout.BeginVertical();
+        var layoutParams = new List<GUILayoutOption>();
+        if (width > 0)
+            layoutParams.Add(GUILayout.MaxWidth(width));
+        if (height > 0)
+            layoutParams.Add(GUILayout.MaxHeight(height));
+        GUILayout.Box(tex, layoutParams.ToArray());
+        GUILayout.Label("'" + tex.name + "'");
+        GUILayout.Label("Size: " + tex.width + "x" + tex.height + "");
+        GUILayout.EndVertical();
+    }
+}
---
+++ Assets/PatternSystem/Nodes/TextureBankNode.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using NodeEditorFramework;
+using System.Collections.Generic;
+
+[Node(false, "Inputs/Textures")]
+public class TextureBankNode : Node
+{
+    public const string ID = "TextureBankNode";
+    public override string GetID => "TextureBankNode";
+    public override string Title => "Textures";
+    public override Vector2 MinSize => new Vector2(120, 200);
+    public override bool AutoLayout => true;
+
+    public List<Texture2D> textures;
+    [System.NonSerialized]
+    private Dictionary<string, ValueConnectionKnob> texKnobs;
+
+
+    public void LoadTextures()
+    {
+        ValueConnectionKnobAttribute outKnobAttribs = new ValueConnectionKnobAttribute("Output", Direction.Out, typeof(Texture));
+        textures = new List<Texture2D>(Resources.LoadAll<Texture2D>("PatternTextures"));
+        texKnobs = new Dictionary<string, ValueConnectionKnob>();
+        foreach (var tex in textures)
+        {
+            texKnobs[tex.name] = CreateValueConnectionKnob(outKnobAttribs);
+        }
+    }
+
+    public override void NodeGUI()
+    {
+
+        GUILayout.BeginVertical();
+        foreach (var tex in textures)
+        {
+            GUILayout.BeginHorizontal();
+            NodeUIElements.TexInfo(tex, height: 64);
+            texKnobs[tex.name].DisplayLayout();
+            GUILayout.EndHorizontal();
+        }
+        GUILayout.EndVertical();
+    }
+
+    public override bool Calculate()
+    {
+        if (textures == null || texKnobs == null)
+        {
+            try
+            {
+                LoadTextures();
+            } catch (UnityException e)
+            {
+                Debug.Log(e+":\n\n"+e.Message);
+            }
+        }
+        foreach (var tex in textures)
+        {
+            if (texKnobs.ContainsKey(tex.name))
+                texKnobs[tex.name].SetValue(tex);
+        }
+        return true;
+    }
+}
