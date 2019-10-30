c880add20832a79d69068f30605eb06b05c6e7d7
Kevin Reynolds <tossrock@gmail.com>
2019-10-29 21:15:53 -0700

    RenderTexture to Texture2D extension method

--- Assets/Scripts/ExtensionMethods.cs
+++ Assets/Scripts/ExtensionMethods.cs
@@ -32,4 +32,13 @@ public static class ExtensionMethods
         if (Time.time < boundary + Time.deltaTime)
             Debug.LogFormat(msg, vals);
     }
+
+    public static Texture2D ToTexture2D(this RenderTexture rTex)
+    {
+        Texture2D tex = new Texture2D(rTex.width, rTex.height, TextureFormat.RGB24, false);
+        RenderTexture.active = rTex;
+        tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
+        tex.Apply();
+        return tex;
+    }
 }
