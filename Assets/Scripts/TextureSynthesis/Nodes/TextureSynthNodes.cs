3e1558d7bba4c2bc26f121a4771858ac3c5bbd20
Kevin Reynolds <tossrock@gmail.com>
2019-12-24 01:08:28 -0800

    Moving abstract Node classes to new TextureSynth namespace

---
+++ Assets/Scripts/TextureSynthesis/Nodes/TextureSynthNodes.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using NodeEditorFramework;
+using NodeEditorFramework.Utilities;
+
+namespace SecretFire.TextureSynth
+{
+    public abstract class TextureSynthNode : Node
+    {
+        protected void KnobOrSlider(ref float val, float min, float max, ValueConnectionKnob knob)
+        {
+            knob.DisplayLayout();
+            if (!knob.connected())
+            {
+                val = RTEditorGUI.Slider(val, min, max);
+            }
+        }
+    }
+
+    public abstract class TickingNode : TextureSynthNode
+    {
+
+    }
+}
