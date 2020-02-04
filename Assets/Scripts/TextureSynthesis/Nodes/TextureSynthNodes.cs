6ba8b10443671e86827645dceab3c97b35026585
Kevin Reynolds <tossrock@gmail.com>
2020-02-04 11:31:56 -0800

    First pass at making graphing available as module to TextureSynth nodes via GraphProvider

--- Assets/Scripts/TextureSynthesis/Nodes/TextureSynthNodes.cs
+++ Assets/Scripts/TextureSynthesis/Nodes/TextureSynthNodes.cs
@@ -4,6 +4,7 @@ using NodeEditorFramework;
 using NodeEditorFramework.Utilities;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 namespace SecretFire.TextureSynth
 {
@@ -159,5 +160,116 @@ namespace SecretFire.TextureSynth
     public abstract class TickingNode : TextureSynthNode
     {
 
+    }
+
+    public class GraphProvider {
+        private List<float> timeValues;
+        private List<float> signalValues;
+
+        private int gridPointsKernel;
+        private int horizontalAxisKernel;
+        private int verticalAxisKernel;
+        private int graphKernel;
+
+        private ComputeShader graphShader;
+        public RenderTexture graphTexture;
+
+        private Vector2Int outputSize;
+
+        private void InitializeRenderTexture()
+        {
+            graphTexture = new RenderTexture(outputSize.x, outputSize.y, 0);
+            graphTexture.enableRandomWrite = true;
+            graphTexture.Create();
+            RenderTexture.active = graphTexture;
+            GL.Clear(false, true, Color.black);
+            RenderTexture.active = null;
+        }
+
+        private void InitializeValues(Vector2Int outputSize){
+            timeValues = new List<float>(257);
+            signalValues = new List<float>(257);
+            graphShader = Resources.Load<ComputeShader>("NodeShaders/GraphView");
+            gridPointsKernel = graphShader.FindKernel("gridPoints");
+            horizontalAxisKernel = graphShader.FindKernel("horizontalAxis");
+            verticalAxisKernel = graphShader.FindKernel("verticalAxis");
+            graphKernel = graphShader.FindKernel("graph");
+            this.outputSize = outputSize;
+            InitializeRenderTexture();
+        }
+
+        public GraphProvider(){
+            InitializeValues(new Vector2Int(256, 256));
+        }
+
+        public GraphProvider(Vector2Int outputSize){
+            InitializeValues(outputSize);
+        }
+
+        public void Update(float x, float y){
+            timeValues.Add(x);
+            signalValues.Add(y);
+            if (timeValues.Count > 255){
+                timeValues.RemoveAt(0);
+                signalValues.RemoveAt(0);
+            }
+        }
+
+        public void Update(float value){
+            Update(Time.time, value);
+        }
+
+        public void DrawGraph()
+        {
+            float windowMaxX = 1, windowMinX = -1, windowMaxY = 1, windowMinY = -1;
+
+            graphShader.SetInt("minTickSpacing", 5);
+            graphShader.SetInts("texSize", outputSize.x, outputSize.y);
+            graphShader.SetFloats("xValues", timeValues.ToArray());
+            graphShader.SetFloats("yValues", signalValues.ToArray());
+            graphShader.SetInt("numPoints", timeValues.Count);
+
+            if (timeValues.Count > 0 && signalValues.Count > 0)
+            {
+                windowMinX = timeValues.Min() - 1;
+                windowMaxX = timeValues.Max() + 1;
+                windowMinY = signalValues.Min() - 1;
+                windowMaxY = signalValues.Max() + 1;
+                graphShader.SetFloats("windowMin", windowMinX, windowMinY);
+                graphShader.SetFloats("windowMax", windowMaxX, windowMaxY);
+            }
+
+            // Set colors
+            graphShader.SetVector("lineColor", Color.cyan);
+            graphShader.SetVector("backgroundColor", new Color(0.1f, 0.1f, 0.1f, 1));
+            graphShader.SetVector("labelColor", Color.white);
+
+            // Set render texture
+            graphShader.SetTexture(graphKernel, "outputTex", graphTexture);
+            graphShader.SetTexture(gridPointsKernel, "outputTex", graphTexture);
+            graphShader.SetTexture(verticalAxisKernel, "outputTex", graphTexture);
+            graphShader.SetTexture(horizontalAxisKernel, "outputTex", graphTexture);
+
+            // Dispatch kernels
+            uint tx, ty, tz;
+            graphShader.GetKernelThreadGroupSizes(gridPointsKernel, out tx, out ty, out tz);
+            var threadGroupX = Mathf.CeilToInt(((float)outputSize.x) / tx);
+            var threadGroupY = Mathf.CeilToInt(((float)outputSize.y) / ty);
+
+            graphShader.Dispatch(gridPointsKernel, threadGroupX, threadGroupY, 1);
+            
+            if (windowMinY < 0 && windowMaxY > 0)
+            {
+                graphShader.Dispatch(horizontalAxisKernel, Mathf.CeilToInt(outputSize.x / 256f), 1, 1);
+            }
+            if (windowMinX < 0 && windowMaxX > 0)
+            {
+                graphShader.Dispatch(verticalAxisKernel, 1, Mathf.CeilToInt(outputSize.y / 256f), 1);
+            }
+            if (signalValues.Count > 0)
+            {
+                graphShader.Dispatch(graphKernel, 1, 1, 1);
+            }
+        }
     }
 }
