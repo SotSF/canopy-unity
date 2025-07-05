8ab34e11cdc86baf0c31aa507c6d8230622a6643
Kevin Reynolds <tossrock@gmail.com>
2025-07-05 00:33:45 -0700

    Add post processing to trails cam, gradient switching

---
+++ Assets/Scripts/TextureSynthesis/Components/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotationSpeed = 0;
+    public Vector3 rotationAxis = Vector3.forward;
+    public Transform rotationTarget;
+
+    void Start()
+    {
+
+    }
+    private void Update()
+    {
+        rotationTarget.transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime, Space.Self);
+    }
+}
